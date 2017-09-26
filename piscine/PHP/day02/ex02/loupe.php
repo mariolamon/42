e4d5b8b17837af3804ef367a721f0df006ae3574
mariolamon <mario.vanhovelamon@gmail.com>
2017-09-26 16:52:27 +0200

    PHP day02/ex02

--- piscine/PHP/day02/ex02/loupe.php
+++ piscine/PHP/day02/ex02/loupe.php
@@ -1,11 +1,13 @@
 #!/usr/bin/php
 <?php
 	if ($argc != 2) exit;
-	else {
-		$file = file_get_contents($argv[1]);
-		$file = preg_replace_callback("/<a href=(.*?)>(.*?)<\/a>/", function($link) {
-			$file = preg_replace_callback(array("/(>)(.*?)(<)/s","/(title=\")(.*?)(\")/s"), strtoupper($match[0]), $link[0]);
-		}, $file);
-		echo $file;
-	}
+	if (!file_exists($argv[1])) exit;
+	$file = file_get_contents($argv[1]);
+	$file = preg_replace_callback('/<a href=(.*?)>(.*?)<\/a>/s', function ($prev_matches) {
+		$file = preg_replace_callback(array('/(>)(.*?)(<)/s', '/(title=")(.*?)(")/s'), function ($matches) {
+			return ($matches[1] . strtoupper($matches[2]) . $matches[3]);
+		}, $prev_matches);
+		return ($file[0]);
+	}, $file);
+	echo $file;
 ?>
