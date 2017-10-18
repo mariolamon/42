3f1381823b324872d218fd985897ff32883e2b8d
mariolamon <mario.vanhovelamon@gmail.com>
2017-10-18 10:26:22 +0200

    PHP day05/ex11

---
+++ piscine/PHP/day05/ex11/ex11.sql
@@ -0,0 +1,1 @@
+SELECT UCASE(fiche_personne.nom) AS 'NOM', `prenom`, `prix` FROM fiche_personne,abonnement,membre WHERE fiche_personne.id_perso = membre.id_fiche_perso AND abonnement.prix > 42 AND abonnement.id_abo = membre.id_abo ORDER BY fiche_personne.nom, fiche_personne.prenom;
