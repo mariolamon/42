3ffe134a71847b5d0d7e57907ad290615cc33115
mariolamon <mario.vanhovelamon@gmail.com>
2018-02-27 12:02:16 +0100

    C exam/2-0-reverse_bits

--- piscine/C/exam/exo/2-0-reverse_bits/reverse_bits.c
+++ piscine/C/exam/exo/2-0-reverse_bits/reverse_bits.c
@@ -1,8 +1,8 @@
 unsigned char	reverse_bits(unsigned char octet) {
-	unsigned char c;
+	unsigned char c = 0;
 	int i;
 	for (i = 0; i < 8; i++) {
-		if (octet & (1 << i)) c = c | (1 << (7 - 1));
+		if (octet & (1 << i)) c |= (1 << (7 - i));
 	}
 	return c;
 }
