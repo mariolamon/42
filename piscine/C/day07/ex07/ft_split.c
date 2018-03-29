252830fe6173d73d58711ea561bfb6f98fc25596
mariolamon <mario.vanhovelamon@gmail.com>
2018-03-29 16:59:58 +0200

    C day07/ex07

--- piscine/C/day07/ex07/ft_split.c
+++ piscine/C/day07/ex07/ft_split.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int	ft_strlen(char *str) {
 	int i = 0;
@@ -6,34 +7,38 @@ int	ft_strlen(char *str) {
 	return (i);
 }
 
-int	ft_test(int i, char *str, char *charset) {
+int	ft_is_charset(int i, char *str, char *charset) {
 	int y;
-	for (y = 0; charset[y]; y++) {
-		if (charset[y] != str[i]) return (0);
-		i++;
-	} 
+	for (y = 0; charset[y]; y++, i++) if (charset[y] != str[i]) return (0);
 	return (1);
+	
 }
 
 int	ft_word_count(char *str, char *charset) {
-	int count = 1, i;
-	for (i = 1; str[i]; i++) {
-		if (ft_test(i, str, charset)) count++;
+	int i = 0, count = 1;
+	while (str[i]) {
+		if (ft_is_charset(i, str, charset)) {
+			count++;
+			i += ft_strlen(charset);
+		}
+		else i++;
 	}
 	return (count);
+
 }
 
 char	**ft_split(char *str, char *charset) {	
 	int word_lenght, y,  x = 0, i = 0;
 	char **result = (char **)malloc(sizeof(char *) * (ft_word_count(str, charset) + 1));
+	
 	while (x < ft_word_count(str, charset)) {
-		for (word_lenght = 0; !(ft_test(i, str, charset) || i == ft_strlen(str)) ; i++) word_lenght++;
+		for (word_lenght = 0; !(ft_is_charset(i, str, charset) || i == ft_strlen(str)) ; i++) word_lenght++;
 		i -= word_lenght;
 		result[x] = (char *)malloc(sizeof(char) * (word_lenght + 1));
-		for (y = 0; y < word_lenght; y++) result[x][y] = str[i++];
-		i += ft_strlen(charset);
+		for (y = 0; y < word_lenght; y++, i++) result[x][y] = str[i];
 		result[x][y] = '\0';
 		x++;
+		i += ft_strlen(charset);
 	}
 	result[ft_word_count(str, charset)] = 0;
 	return (result);
