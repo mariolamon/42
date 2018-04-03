c8fe66c636ff06343d02e49b5e5414b6d4ba7888
mariolamon <mario.vanhovelamon@gmail.com>
2018-04-03 15:04:19 +0200

    C: day08/ex01

--- piscine/C/day08/ex01/ft_boolean.h
+++ piscine/C/day08/ex01/ft_boolean.h
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 #ifndef	FT_BOOLEAN_H
+#define FT_BOOLEAN_H
 	#define TRUE		1
 	#define FALSE		0
 	#define SUCCESS		1
 	#define EVEN_MSG	"J'ai un nombre pair d'argument.\n"
 	#define ODD_MSG		"J'ai un nombre impair d'argument.\n"
-	#define EVEN(x)		(x % 2 ? TRUE : FALSE)
-	
+	#define EVEN(x)		(x % 2 ? TRUE : FALSE)	
 	typedef int t_bool;
 #endif
