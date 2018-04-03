f5ee753f5b48ec9eba872fbe21d5cb5a53f7422c
mariolamon <mario.vanhovelamon@gmail.com>
2018-04-03 12:05:20 +0200

    C: day08/ex00

---
+++ piscine/C/day08/ex00/ft.h
@@ -0,0 +1,6 @@
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int	ft_strcmp(char *s1, char *s2);
+char	*ft_strcpy(char *dest, char *src);
+int	ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
